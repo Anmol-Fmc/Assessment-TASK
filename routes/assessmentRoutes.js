2fb7ccf8ab41c4ae1d7ce9aa55a171702586e01c
Anmol-Fmc <software3@fathhomAI.onmicrosoft.com>
2025-12-16 18:36:43 +0530

    Initial commit

---
+++ routes/assessmentRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const AssessmentController= require("../controllers/assessmentController");
+
+const router = express.Router();
+
+router.post("/",AssessmentController.create);
+router.get("/:id",AssessmentController.getById);
+router.put("/:id",AssessmentController.updateById);
+router.delete("/:id",AssessmentController.deleteById);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const assessmentRoutes = require ("./routes/assessmentRoutes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/assessments",assessmentRoutes);
+
+app.get("/",(req,res)=>{
+    res.json({status:"API is running"});
+});
+
+const PORT = process.env.PORT||5432;
+
+app.listen(PORT,()=>{
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+
+});
